801c2de6e0c2764b637fb3d6d0f29e6ed2e0b596
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2024-09-04 09:43:59 +0100

    [Bug] SC-124017 fix height scrolling

--- src/pages/Admin/GlobalAuth.tsx
+++ src/pages/Admin/GlobalAuth.tsx
@@ -14,7 +14,7 @@ export const GlobalAuth = () => {
   const { callbackUrl, signIn, message, authUrl } = useGlobalAuth();
 
   return (
-    <Stack vertical gap={10} style={{ margin: "0px", height: "1000px" }}>
+    <Stack vertical gap={10} style={{ margin: "0px" }}>
       {callbackUrl && (
         <>
           <H2 style={{ marginBottom: "5px" }}>Callback URL</H2>
