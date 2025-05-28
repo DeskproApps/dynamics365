17ddd26f1fe704a480f3cac39db94f2c090fc806
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 11:23:12 +0000

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/react';
+import './instrument';
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
@@ -5,7 +7,6 @@ import {
   QueryClientProvider,
   QueryErrorResetBoundary,
 } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import { DeskproAppProvider, LoadingSpinner } from "@deskpro/app-sdk";
 import { queryClient } from "./query";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
@@ -27,12 +28,12 @@ root.render(
             <Suspense fallback={<LoadingSpinner />}>
               <QueryErrorResetBoundary>
                 {({ reset }) => (
-                  <ErrorBoundary
+                  <Sentry.ErrorBoundary
                     onReset={reset}
                     FallbackComponent={ErrorFallback}
                   >
                     <App />
-                  </ErrorBoundary>
+                  </Sentry.ErrorBoundary>
                 )}
               </QueryErrorResetBoundary>
             </Suspense>
